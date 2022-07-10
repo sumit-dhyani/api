7f6137c632ce7aaa2029084ca48ad87c8d6152f9
sumit-dhyani <dhyanisumit3@gmail.com>
2022-07-10 18:29:36 +0530

    final changes

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express=require('express')
 const axios=require('axios').default
-// const app=express()
+const app=express()
 const cors=require('cors')
-const fastify = require('fastify')({ logger: true })
-// app.use(cors())
-const router=express.Router()
-router.use(cors())
-router.get('/',(req,res)=>{
+app.use(cors())
+
+app.get('/',(req,res)=>{
     axios.get("https://fakestoreapi.com/products")
     .then(response=>{   
         res.json(response.data)
@@ -17,18 +15,18 @@ router.get('/',(req,res)=>{
 const PORT=process.env.PORT||5500
 
 
-// app.listen(PORT,function(){
-//     console.log(`server is running at port : ${PORT}`)
-// })
+app.listen(PORT,function(){
+    console.log(`server is running at port : ${PORT}`)
+})
 
 
-fastify.register(require('@fastify/express'))
-  .after(() => {
-    // fastify.use(express.urlencoded({extended: false})) // for Postman x-www-form-urlencoded
-    // fastify.use(express.json())
+// fastify.register(require('@fastify/express'))
+//   .after(() => {
+//     // fastify.use(express.urlencoded({extended: false})) // for Postman x-www-form-urlencoded
+//     // fastify.use(express.json())
 
-    fastify.use(router)
-    // 7599571929
-  })
+//     fastify.use(router)
+//     // 7599571929
+//   })
 
-fastify.listen(PORT, console.log)
+// fastify.listen(PORT, console.log)
